b2b5cb10347c6630d2a45af2ac04024125e15983
Nvaa <130564362+ItsNvaa@users.noreply.github.com>
2024-04-27 22:55:47 -0400

    [MUS-20] Load Application Fonts (#22)
    * Removed default static font
    
    * Added plus jakarta sans static fonts
    
    * Added inter static fonts
    
    * Loaded application fonts
    
    * Added constant fonts list
    
    ---------
    
    Co-authored-by: ItsNvaa <kadeknovacahataputra@gmail.com>

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,20 @@
-import { Slot } from "expo-router";
+import fonts from "@/constants/fonts";
+import { useFonts } from "expo-font";
+import { Slot, SplashScreen } from "expo-router";
+import React from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts(fonts);
+
+  React.useEffect(() => {
+    if (error) throw error;
+
+    if (loaded) SplashScreen.hideAsync();
+  }, [loaded]);
+
+  if (!loaded && !error) return null;
+
   return <Slot />;
 }
---
+++ constants/fonts.ts
@@ -0,0 +1,8 @@
+export default {
+  regular: require("@/assets/fonts/jakarta-sans/PlusJakartaSans-Regular.ttf"),
+  bold: require("@/assets/fonts/jakarta-sans/PlusJakartaSans-Bold.ttf"),
+  medium: require("@/assets/fonts/jakarta-sans/PlusJakartaSans-Medium.ttf"),
+  light: require("@/assets/fonts/jakarta-sans/PlusJakartaSans-Light.ttf"),
+  extraLight: require("@/assets/fonts/jakarta-sans/PlusJakartaSans-ExtraLight.ttf"),
+  extraBold: require("@/assets/fonts/jakarta-sans/PlusJakartaSans-ExtraBold.ttf"),
+};
