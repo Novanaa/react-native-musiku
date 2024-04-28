980ed58cd589d4b43f1f23a7122e31deb7614ce6
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-04-28 07:08:18 -0400

    Setted application dark theme background

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,14 @@ import fonts from "@/constants/fonts";
 import useDefaultTheme from "@/hooks/use-default-theme";
 import { useFonts } from "expo-font";
 import { Slot, SplashScreen } from "expo-router";
-import { Appearance } from "react-native";
+import {
+  Appearance,
+  ColorSchemeName,
+  StyleSheet,
+  View,
+  useColorScheme,
+} from "react-native";
+import colors from "@/constants/colors";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -20,5 +27,20 @@ export default function RootLayout() {
 
   if (!loaded && !error) return null;
 
-  return <Slot />;
+  return (
+    <View style={styles.container}>
+      <Slot />
+    </View>
+  );
 }
+
+const appTheme: ColorSchemeName = Appearance.getColorScheme();
+const backgroundColor: string =
+  appTheme == "light" ? colors.light.background : colors.dark.background;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor,
+    flex: 1,
+  },
+});
