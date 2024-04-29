240de42dce4b3b677b8d44a0ddfa0f16f42ca226
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-04-29 05:06:02 -0400

    Added application background color

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,10 @@
-import { Text, View } from "react-native";
+import Text from "@/components/atomics/text";
+import { backgroundColor } from "@/constants/colors";
+import { View } from "react-native";
 
 export default function Home() {
   return (
-    <View>
+    <View style={{ backgroundColor, flex: 1 }}>
       <Text>Hello, World!</Text>
     </View>
   );
--- app/search.tsx
+++ app/search.tsx
@@ -1,10 +1,11 @@
 import Text from "@/components/atomics/text";
+import { backgroundColor } from "@/constants/colors";
 import React from "react";
 import { View } from "react-native";
 
 export default function SearchScreen(): React.JSX.Element {
   return (
-    <View>
+    <View style={{ backgroundColor, flex: 1 }}>
       <Text>Search</Text>
     </View>
   );
