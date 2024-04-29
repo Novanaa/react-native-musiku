91470af1ad97a1fe376cec9208257ec879b83649
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-04-29 05:36:21 -0400

    Added application tabs layout

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,7 @@
+import BottomTabs from "@/components/molecules/bottom-tabs";
+import React from "react";
+import { View } from "react-native";
+
+export default function TabsLayout(): React.JSX.Element {
+  return <BottomTabs />;
+}
