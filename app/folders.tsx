aa65d72f120a6dca9dd6dce678c783a703de5e65
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-04-29 04:24:38 -0400

    Added application folders screen

---
+++ app/folders.tsx
@@ -0,0 +1,11 @@
+import Text from "@/components/atomics/text";
+import React from "react";
+import { View } from "react-native";
+
+export default function FoldersScreen(): React.JSX.Element {
+  return (
+    <View>
+      <Text>Folders</Text>
+    </View>
+  );
+}
