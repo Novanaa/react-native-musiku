8badaf66f7a6d443a7a4edc72a045a97f36cf7b3
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-04-29 05:38:35 -0400

    Removed unused import module

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,5 @@
 import BottomTabs from "@/components/molecules/bottom-tabs";
 import React from "react";
-import { View } from "react-native";
 
 export default function TabsLayout(): React.JSX.Element {
   return <BottomTabs />;
