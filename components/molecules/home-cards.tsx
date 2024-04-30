327b6213a69542821596e4f423af06a2676b84ce
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-04-30 10:01:52 -0400

    Added icon property to card component

--- components/molecules/home-cards.tsx
+++ components/molecules/home-cards.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { FlatList, Linking } from "react-native";
-import cardsData from "@/resources/cards.json";
 import Card from "../atomics/card";
 import { rowsGap } from "@/constants/styles";
+import { homeCardsData } from "@/resources/cards";
 
 export default function HomeCards(): React.JSX.Element {
   return (
     <FlatList
-      data={cardsData}
+      data={homeCardsData}
       renderItem={({ item }) => (
         <Card
           title={item.title}
           onPress={() => Linking.openURL(item.link)}
           description={item.description}
+          icon={item.icon}
         />
       )}
       horizontal
