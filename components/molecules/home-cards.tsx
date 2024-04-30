d5611e7540cd064196deb330d0ec3166cfd96a38
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-04-30 08:42:19 -0400

    Changed from react router system to external linking

--- components/molecules/home-cards.tsx
+++ components/molecules/home-cards.tsx
@@ -1,22 +1,15 @@
 import React from "react";
-import { FlatList } from "react-native";
+import { FlatList, Linking } from "react-native";
 import cardsData from "@/resources/cards.json";
 import Card from "../atomics/card";
 import { rowsGap } from "@/constants/styles";
-import { Router, useRouter } from "expo-router";
-import { RelativePathString } from "@/app/app";
 
 export default function HomeCards(): React.JSX.Element {
-  const router: Router = useRouter();
-
   return (
     <FlatList
       data={cardsData}
       renderItem={({ item }) => (
-        <Card
-          title={item.title}
-          onPress={() => router.push(item.link as RelativePathString)}
-        />
+        <Card title={item.title} onPress={() => Linking.openURL(item.link)} />
       )}
       horizontal
       contentContainerStyle={{ gap: rowsGap }}
