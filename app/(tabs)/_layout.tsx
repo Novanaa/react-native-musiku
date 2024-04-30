d50cfd4a7e39b7d40f9013527bdf51b7ab441131
Nvaa <130564362+ItsNvaa@users.noreply.github.com>
2024-04-30 06:29:40 -0400

    [MUS-32] Implement "Application Navigation Bar" Feature (#48)
    * Added empty react component called 'scan'
    
    * Added scan music application icon assest
    
    * Added scan music application icon assets
    
    * Added application header to tabs layout
    
    * Added icon button atomic react component
    
    * Added application header feature
    
    ---------
    
    Co-authored-by: ItsNvaa <kadeknovacahataputra@gmail.com>

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,12 @@
 import BottomTabs from "@/components/molecules/bottom-tabs";
+import Header from "@/components/molecules/header";
 import React from "react";
 
 export default function TabsLayout(): React.JSX.Element {
-  return <BottomTabs />;
+  return (
+    <>
+      <Header />
+      <BottomTabs />
+    </>
+  );
 }
