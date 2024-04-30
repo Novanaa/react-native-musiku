bd0af2c619b090de61eb3958799c1fa9aa14f45c
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-04-30 07:57:11 -0400

    Removed application header padding horizontal style

--- components/molecules/header.tsx
+++ components/molecules/header.tsx
@@ -23,7 +23,6 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: 60,
     paddingBottom: 20,
-    paddingHorizontal: 20,
     justifyContent: "space-between",
     alignItems: "center",
     display: "flex",
