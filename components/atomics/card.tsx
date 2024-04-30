49e6d799b8a540c9d3edb84de5229ed0d4b22e40
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-04-30 10:02:47 -0400

    Added react atomic card component

---
+++ components/atomics/card.tsx
@@ -0,0 +1,51 @@
+import {
+  StyleSheet,
+  TouchableOpacityProps,
+  TouchableOpacity,
+  View,
+  Image,
+  ImageSourcePropType,
+} from "react-native";
+import React from "react";
+import Text from "./text";
+import { borderColor } from "@/constants/colors";
+import { borderRadius } from "@/constants/styles";
+
+interface CardProps extends TouchableOpacityProps {
+  title: string;
+  description: string;
+  icon: ImageSourcePropType;
+}
+
+export default function Card(props: CardProps): React.JSX.Element {
+  return (
+    <TouchableOpacity style={styles.container} onPress={props.onPress}>
+      <View>
+        <Image source={props.icon} />
+        <Text style={styles.title}>{props.title}</Text>
+      </View>
+      <Text style={styles.description}>{props.description}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 210,
+    height: 110,
+    borderColor,
+    borderWidth: 1,
+    borderRadius,
+    padding: 10,
+    backgroundColor: "#1a1919",
+  },
+  title: {
+    fontFamily: "bold",
+    fontSize: 15,
+  },
+  description: {
+    fontSize: 12,
+    fontFamily: "medium",
+    opacity: 0.75,
+  },
+});
