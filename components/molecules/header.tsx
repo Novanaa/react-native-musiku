9783602aa2f203e0ba408ebaff0ef5f8771f2401
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-04-30 06:28:47 -0400

    Added application header feature

---
+++ components/molecules/header.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import Text from "../atomics/text";
+import assests from "@/constants/assests";
+import { IconButton } from "../atomics/button";
+import { Router, useRouter } from "expo-router";
+
+export default function Header(): React.JSX.Element {
+  const router: Router = useRouter();
+  return (
+    <View style={styles.container}>
+      <Text style={styles.appName}>Musiku</Text>
+      <IconButton
+        icon={assests.icons.scanMusic}
+        alt="Scan your music"
+        onPress={() => router.push("/scan")}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 60,
+    paddingBottom: 20,
+    paddingHorizontal: 20,
+    justifyContent: "space-between",
+    alignItems: "center",
+    display: "flex",
+    flexDirection: "row",
+  },
+  appName: {
+    fontFamily: "bold",
+    fontSize: 18,
+  },
+});
