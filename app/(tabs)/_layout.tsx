31afb38801627672ce84176c5fb99161cb591370
Nvaa <130564362+ItsNvaa@users.noreply.github.com>
2024-04-30 07:26:55 -0400

    [MUS-34] Remove Application Header From Folders and Search Tabs (#51)
    Co-authored-by: ItsNvaa <kadeknovacahataputra@gmail.com>

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,9 @@
 import BottomTabs from "@/components/molecules/bottom-tabs";
-import Header from "@/components/molecules/header";
 import React from "react";
 
 export default function TabsLayout(): React.JSX.Element {
   return (
     <>
-      <Header />
       <BottomTabs />
     </>
   );
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
 import Text from "@/components/atomics/text";
 import { View } from "react-native";
 import { backgroundColor } from "@/constants/colors";
+import Header from "@/components/molecules/header";
 
 export default function Home() {
   return (
     <View style={{ backgroundColor, flex: 1 }}>
+      <Header />
       <Text>Hello, World!</Text>
     </View>
   );
