f30ff5793ad3ccfdff76026e2afbe47a9aa22332
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-04-30 06:23:30 -0400

    Added scan music application icon assest

--- constants/assests.ts
+++ constants/assests.ts
@@ -4,5 +4,6 @@ export default {
     home: require("../assets/icons/home.png"),
     search: require("../assets/icons/magnifying-glass.png"),
     app: require("../assets/icons/musiku.png"),
+    scanMusic: require("../assets/icons/scan-music.png"),
   },
 };
