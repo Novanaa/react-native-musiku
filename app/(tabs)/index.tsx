4953c7a4e4397fba9bb76e63ead58c189f09f635
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-04-30 22:41:30 -0400

    Implemented music list header

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,14 @@
 import Header from "@/components/molecules/header";
 import Container from "@/components/atomics/container";
 import HomeCards from "@/components/molecules/home-cards";
+import MusicListHeader from "@/components/molecules/music-list-header";
 
 export default function Home() {
   return (
     <Container>
       <Header />
       <HomeCards />
+      <MusicListHeader />
     </Container>
   );
 }
