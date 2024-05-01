d2236d195e0b9c6e9eaa75389aa56e5282b48e39
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-04-30 20:13:12 -0400

    Updated tabs container

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,10 @@
-import { backgroundColor } from "@/constants/colors";
 import Header from "@/components/molecules/header";
 import Container from "@/components/atomics/container";
 import HomeCards from "@/components/molecules/home-cards";
 
 export default function Home() {
   return (
-    <Container style={{ backgroundColor, flex: 1 }}>
+    <Container>
       <Header />
       <HomeCards />
     </Container>
--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,12 +1,11 @@
+import Container from "@/components/atomics/container";
 import Text from "@/components/atomics/text";
 import React from "react";
-import { View } from "react-native";
-import { backgroundColor } from "@/constants/colors";
 
 export default function SearchScreen(): React.JSX.Element {
   return (
-    <View style={{ backgroundColor, flex: 1 }}>
+    <Container>
       <Text>Search</Text>
-    </View>
+    </Container>
   );
 }
