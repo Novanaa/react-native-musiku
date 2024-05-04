56ff4f00d4cc6181d49b3b6f2d3c10b43b49192a
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-05-04 08:26:39 -0400

    Removed react atomic light mode styles component

--- components/atomics/text.tsx
+++ components/atomics/text.tsx
@@ -1,23 +1,15 @@
-import { Text as NativeText, StyleSheet, TextProps } from "react-native";
+import { Text as NativeText, TextProps } from "react-native";
 import React from "react";
 import colors from "@/constants/colors";
-import { isLightTheme } from "@/utils/app-theme";
 
 export default function Text(props: TextProps): React.JSX.Element {
-  const themeStyle = isLightTheme() ? styles.light : styles.dark;
-
   return (
-    <NativeText style={[themeStyle, props.style]}>{props.children}</NativeText>
+    <NativeText
+      style={[{ fontFamily: "regular", color: colors.dark.text }, props.style]}
+      numberOfLines={props.numberOfLines}
+      ellipsizeMode={props.ellipsizeMode}
+    >
+      {props.children}
+    </NativeText>
   );
 }
-
-const styles = StyleSheet.create({
-  light: {
-    fontFamily: "regular",
-    color: colors.light.text,
-  },
-  dark: {
-    fontFamily: "regular",
-    color: colors.dark.text,
-  },
-});
