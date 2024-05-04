c81378e950ab015f11d3fd689d8feae1e7263af3
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-05-04 08:34:22 -0400

    Added music list react molucules component

---
+++ components/molecules/music-list.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Music from "../atomics/music";
+import { FlatList, StyleSheet } from "react-native";
+
+export default function MusicList(): React.JSX.Element {
+  return (
+    <FlatList
+      // User songs data goes here!
+      data={[]}
+      renderItem={() => (
+        <Music
+          // Just a placeholder text for user songs
+          description="Unknown Artist - Unknown Album"
+          title="Sufjan Stevens - The Black Hawk War, or, How to Demolish an Entire Civilization and Still Feel Good About Yourself in the Morning, or, We Apologize for the Inconvenience but You're Going to Have to Leave Now, or, 'I Have Fought the Big Knives and Will Continue to Fight Them Until They Are Off Our Lands!"
+        />
+      )}
+      style={styles.container}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 8,
+    marginBottom: 8,
+    height: 100,
+  },
+});
