dcbf8b98a253c7380ce4f360d23dfdfa6f4691a9
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-05-06 00:46:57 -0400

    Added new music search not found react molecules component

--- components/molecules/not-found.tsx
+++ components/molecules/not-found.tsx
@@ -7,23 +7,37 @@ import assests from "@/constants/assests";
 export function MusicNotDetected(): React.JSX.Element {
   const router: Router = useRouter();
   return (
-    <View style={styles.container}>
+    <View style={musicNotDetectedStyles.container}>
       <Image
         source={assests.images.musicDisc}
         alt="music-not-detected"
-        style={styles.image}
+        style={musicNotDetectedStyles.image}
       />
-      <Text style={styles.messege}>
+      <Text style={musicNotDetectedStyles.messege}>
         Music Not Detected, Are You Ready to Listen Your Favorite Music?
       </Text>
       <TouchableOpacity onPress={() => router.push("/scan")}>
-        <Text style={styles.cta}>Scan Your Music</Text>
+        <Text style={musicNotDetectedStyles.cta}>Scan Your Music</Text>
       </TouchableOpacity>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+export function MusicSearchNotFound(): React.JSX.Element {
+  return (
+    <View style={musicSearchNotFoundStyles.container}>
+      <Text style={musicSearchNotFoundStyles.notFoundText}>404</Text>
+      <Text style={musicSearchNotFoundStyles.headerText}>
+        Not results found
+      </Text>
+      <Text style={musicSearchNotFoundStyles.messege}>
+        Requested music cannot be found, try searching something else.
+      </Text>
+    </View>
+  );
+}
+
+const musicNotDetectedStyles = StyleSheet.create({
   container: {
     justifyContent: "center",
     alignItems: "center",
@@ -42,5 +56,27 @@ const styles = StyleSheet.create({
   },
   cta: {
     fontFamily: "bold",
+  },
+});
+
+const musicSearchNotFoundStyles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+    gap: 5,
+    opacity: 0.85,
+  },
+  notFoundText: {
+    fontFamily: "extraBold",
+  },
+  headerText: {
+    fontFamily: "medium",
+    fontSize: 16,
+  },
+  messege: {
+    textAlign: "center",
+    width: "70%",
+    opacity: 0.8,
   },
 });
