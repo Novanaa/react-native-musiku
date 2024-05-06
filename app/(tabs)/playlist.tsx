04a8b8c05cbaf5e3d1407f81843e925e8a4ef0f8
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-05-06 00:02:35 -0400

    Added application playlist tabs

---
+++ app/(tabs)/playlist.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Container from "@/components/atomics/container";
+import Text from "@/components/atomics/text";
+
+export default function Playlist(): React.JSX.Element {
+  return (
+    <Container>
+      <Text>Playlist</Text>
+    </Container>
+  );
+}
