d600a38dd633285813c50d913c7a38d2f1d3be3a
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-05-06 00:47:31 -0400

    Added music not found placeholder

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,5 +1,6 @@
 import Container from "@/components/atomics/container";
 import SearchBar from "@/components/atomics/search-bar";
+import { MusicSearchNotFound } from "@/components/molecules/not-found";
 import React from "react";
 import { StyleSheet } from "react-native";
 
@@ -7,6 +8,9 @@ export default function SearchScreen(): React.JSX.Element {
   return (
     <Container style={styles.container}>
       <SearchBar />
+      {/* Change it later on! */}
+      <MusicSearchNotFound />
+      {/* Change it later on! */}
     </Container>
   );
 }
