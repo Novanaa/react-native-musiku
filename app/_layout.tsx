30b3fd1528f5727ecf22dedf008854c59af83222
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-05-08 03:16:36 -0400

    Fixed uncorrect component import

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,7 +9,7 @@ import { backgroundColor } from "@/constants/colors";
 import { StatusBar } from "expo-status-bar";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
-import { UserPermission } from "@/components/molecules/user-permission";
+import { UserPermissionProvider } from "@/providers/user-permission";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -29,12 +29,12 @@ export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <BottomSheetModalProvider>
-        <UserPermission>
+        <UserPermissionProvider>
           <StatusBar style="dark" />
           <Stack screenOptions={{ contentStyle: styles.container }}>
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           </Stack>
-        </UserPermission>
+        </UserPermissionProvider>
       </BottomSheetModalProvider>
     </GestureHandlerRootView>
   );
