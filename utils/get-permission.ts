eda93204015e96adc5ebdcfc5da51dd5082ccf55
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-05-08 20:51:15 -0400

    Added a several validation for user get permission feature

--- utils/get-permission.ts
+++ utils/get-permission.ts
@@ -1,17 +1,27 @@
 import * as MediaLibrary from "expo-media-library";
 import { Linking } from "react-native";
 
-export default async function getPermission(): Promise<void> {
+export default async function getPermission(
+  setterState: React.Dispatch<React.SetStateAction<boolean>>
+): Promise<void> {
   const permission: Awaited<MediaLibrary.PermissionResponse> =
     await MediaLibrary.getPermissionsAsync();
 
   if (!permission.granted && !permission.canAskAgain) Linking.openSettings();
 
   if (!permission.granted && permission.canAskAgain) {
-    await MediaLibrary.requestPermissionsAsync();
+    const requestedPermissions: Awaited<MediaLibrary.PermissionResponse> =
+      await MediaLibrary.requestPermissionsAsync();
+
+    if (!requestedPermissions.granted && !requestedPermissions.canAskAgain)
+      Linking.openSettings();
+
+    if (permission.granted) setterState(true);
 
     return;
   }
 
-  await MediaLibrary.requestPermissionsAsync();
+  if (permission.granted) setterState(true);
+
+  return;
 }
