161dbcbd95bc87037de876373c7be63efcc09417
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-05-10 02:19:08 -0400

    Refactored some lines of codes

--- components/molecules/folder-list.tsx
+++ components/molecules/folder-list.tsx
@@ -2,43 +2,20 @@ import React from "react";
 import { ScrollView, StyleSheet, View } from "react-native";
 import Folder from "../atomics/folder";
 import { EmptyMusic, MusicNotDetected } from "./not-found";
-import { MusicContext, MusicContextData } from "@/providers/music-provider";
-
-interface FolderData {
-  path: string;
-  folderName: string;
-}
+import { FolderContext, FolderContextData } from "@/providers/folder-provider";
 
 export default function FolderList(): React.JSX.Element {
-  const musicContext: MusicContextData = React.useContext(MusicContext);
-
-  // Validate if the user music is empty
-  if (!musicContext?.totalCount) return <EmptyMusic />;
+  const folderContext: FolderContextData = React.useContext(FolderContext);
 
   // Validate if the user music is not detected
-  if (musicContext == null) return <MusicNotDetected />;
+  if (folderContext == null) return <MusicNotDetected />;
 
-  const folder: Array<FolderData> = musicContext.assets
-    .map((item) => {
-      // Map music data uri
-      const splitedFilePath: Array<string> = item.uri.split("/");
-      const folderName: string = splitedFilePath[splitedFilePath.length - 2];
-      const path: string = item.uri.substring(0, item.uri.lastIndexOf("/"));
-
-      return {
-        folderName,
-        path,
-      };
-    })
-    .filter(
-      // To removes duplicates array datas
-      (item, index, self) =>
-        index === self.findIndex((t) => t.path === item.path)
-    );
+  // Validate if the user music is empty
+  if (!folderContext.length) return <EmptyMusic />;
 
   return (
     <ScrollView style={styles.container}>
-      {folder.map((item, i) => (
+      {folderContext.map((item, i) => (
         <View style={styles.wrapper}>
           <Folder title={item.folderName} description={item.path} key={i} />
         </View>
