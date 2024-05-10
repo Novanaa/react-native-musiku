d12f069e5a47bb36c681dadb1c3dd7309e5fa22b
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-05-09 23:20:31 -0400

    Fixed eslint conflicts

--- utils/get-music.tsx
+++ utils/get-music.tsx
@@ -2,7 +2,7 @@ import * as MediaLibrary from "expo-media-library";
 
 export default async function getMusic(): Promise<
   MediaLibrary.PagedInfo<MediaLibrary.Asset>
-> {
+  > {
   const pagedMusic: Awaited<MediaLibrary.PagedInfo<MediaLibrary.Asset>> =
     await MediaLibrary.getAssetsAsync({ mediaType: "audio" });
 
