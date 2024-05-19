cdeab831a23552f96f0e904b40e8591169a1e049
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-05-19 05:49:47 -0400

    Removed app theme utils

--- utils/app-theme.ts
+++
@@ -1,9 +0,0 @@
-import { Appearance } from "react-native";
-
-export function isDarkTheme(): boolean {
-  return Appearance.getColorScheme() == "dark";
-}
-
-export function isLightTheme(): boolean {
-  return Appearance.getColorScheme() == "light";
-}
