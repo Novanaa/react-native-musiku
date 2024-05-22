ced6787a94f3aa6bba71fe237649bcc0a8fea993
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-05-22 06:24:39 -0400

    Validated music sort list

--- utils/get-music.ts
+++ utils/get-music.ts
@@ -1,3 +1,4 @@
+import SortByRepository from "@/repository/sort-by.repository";
 import * as MediaLibrary from "expo-media-library";
 
 export default async function getMusic(): Promise<
@@ -6,8 +7,30 @@ export default async function getMusic(): Promise<
   const pagedMusic: Awaited<MediaLibrary.PagedInfo<MediaLibrary.Asset>> =
     await MediaLibrary.getAssetsAsync({ mediaType: "audio" });
 
-  return await MediaLibrary.getAssetsAsync({
-    mediaType: "audio",
-    first: pagedMusic.totalCount,
-  });
+  const allFetchedMusic: Awaited<MediaLibrary.PagedInfo<MediaLibrary.Asset>> =
+    await MediaLibrary.getAssetsAsync({
+      mediaType: "audio",
+      first: pagedMusic.totalCount,
+      sortBy: MediaLibrary.SortBy.duration,
+    });
+
+  const defaultSortedMusic: MediaLibrary.PagedInfo<MediaLibrary.Asset> = {
+    ...allFetchedMusic,
+    assets: allFetchedMusic.assets.sort((a, b) =>
+      a.filename.localeCompare(b.filename)
+    ),
+  };
+
+  if (SortByRepository.getSortByState() == "ascending")
+    return defaultSortedMusic;
+
+  if (SortByRepository.getSortByState() == "descending")
+    return {
+      ...allFetchedMusic,
+      assets: allFetchedMusic.assets.sort((a, b) =>
+        b.filename.localeCompare(a.filename)
+      ),
+    };
+
+  return defaultSortedMusic;
 }
