cbe1c28e048953495625d32274add72af8333aed
Nvaa <130564362+ItsNvaa@users.noreply.github.com>
2024-05-22 18:45:36 -0400

    [MUS-99] Add Favorite Songs Playlist (#175)
    * Added react favorite playlist atomic component
    
    * Rendered favorite playlist component
    
    ---------
    
    Co-authored-by: ItsNvaa <kadeknovacahataputra@gmail.com>

--- app/(tabs)/playlist.tsx
+++ app/(tabs)/playlist.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Container from "@/components/atomics/container";
-import Text from "@/components/atomics/text";
+import FavoritePlaylist from "@/components/atomics/favorite-playlist";
+import { StyleSheet } from "react-native";
 
 export default function Playlist(): React.JSX.Element {
   return (
-    <Container>
-      <Text>Playlist</Text>
+    <Container style={styles.container}>
+      <FavoritePlaylist />
     </Container>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 5,
+  },
+});
