5737a4867f036e85203d8c0d1ba2c1126e593d95
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-05-22 06:26:19 -0400

    Refactored some line of code

--- utils/get-music.ts
+++ utils/get-music.ts
@@ -3,7 +3,7 @@ import * as MediaLibrary from "expo-media-library";
 
 export default async function getMusic(): Promise<
   MediaLibrary.PagedInfo<MediaLibrary.Asset>
-  > {
+> {
   const pagedMusic: Awaited<MediaLibrary.PagedInfo<MediaLibrary.Asset>> =
     await MediaLibrary.getAssetsAsync({ mediaType: "audio" });
 
@@ -14,15 +14,13 @@ export default async function getMusic(): Promise<
       sortBy: MediaLibrary.SortBy.duration,
     });
 
-  const defaultSortedMusic: MediaLibrary.PagedInfo<MediaLibrary.Asset> = {
-    ...allFetchedMusic,
-    assets: allFetchedMusic.assets.sort((a, b) =>
-      a.filename.localeCompare(b.filename)
-    ),
-  };
-
   if (SortByRepository.getSortByState() == "ascending")
-    return defaultSortedMusic;
+    return {
+      ...allFetchedMusic,
+      assets: allFetchedMusic.assets.sort((a, b) =>
+        a.filename.localeCompare(b.filename)
+      ),
+    };
 
   if (SortByRepository.getSortByState() == "descending")
     return {
@@ -32,5 +30,10 @@ export default async function getMusic(): Promise<
       ),
     };
 
-  return defaultSortedMusic;
+  return {
+    ...allFetchedMusic,
+    assets: allFetchedMusic.assets.sort((a, b) =>
+      a.filename.localeCompare(b.filename)
+    ),
+  };
 }
