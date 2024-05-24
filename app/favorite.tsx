b9313bc01c4e83e0abda44529f8c45ec81f082b6
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-05-24 08:22:43 -0400

    Validated if favorited music is empty

--- app/favorite.tsx
+++ app/favorite.tsx
@@ -1,6 +1,15 @@
+import { EmptyFavoritesMusic } from "@/components/molecules/not-found";
+import { default as IFavorites } from "@/interfaces/favorites";
+import { useFavoritesMusic } from "@/stores/favorites";
 import React from "react";
 import { View } from "react-native";
 
 export default function Favorites(): React.JSX.Element {
+  const favoritedMusic: IFavorites = useFavoritesMusic((state) =>
+    JSON.parse(state.favorites)
+  );
+
+  if (!favoritedMusic.total) return <EmptyFavoritesMusic />;
+
   return <View></View>;
 }
