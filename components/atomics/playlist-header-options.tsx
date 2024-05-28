8cbd656401f9cd4af513b19f2837230fde3eb9f5
Novaa <130564362+ItsNvaa@users.noreply.github.com>
2024-05-28 10:40:04 -0400

    [MUS-123] Add Removes All Playlist Feature (#221)
    * Implemented removes all playlist feature
    
    * Added reset playlist application utils
    
    * Added removes all playlist drawer component
    
    * Disabled removes playlist button when user playlist is empty
    
    ---------
    
    Co-authored-by: ItsNvaa <kadeknovacahataputra@gmail.com>

--- components/atomics/playlist-header-options.tsx
+++ components/atomics/playlist-header-options.tsx
@@ -16,6 +16,10 @@ import AlbumSVG from "@/assets/icons/album.svg";
 import HeartSVG from "@/assets/icons/heart.svg";
 import { useRouter } from "expo-router";
 import { ExpoRouter } from "expo-router/types/expo-router";
+import { RemovesAllPlaylist } from "./playlist-header-options-actions";
+import { BottomSheetModalMethods } from "@gorhom/bottom-sheet/lib/typescript/types";
+import { usePlaylistStore } from "@/stores/playlist";
+import { PlaylistScheme } from "@/interfaces/playlist";
 
 interface PlaylistHeaderOptionsItemProps extends TouchableHighlightProps {
   icon: React.FC<SvgProps>;
@@ -26,34 +30,47 @@ interface PlaylistHeaderOptionsItemProps extends TouchableHighlightProps {
 export default function PlaylistHeaderOptions(
   props: DrawerProps
 ): React.JSX.Element {
+  const list: PlaylistScheme = usePlaylistStore((state) =>
+    JSON.parse(state.playlist)
+  );
+  const removesPlaylistDrawerRef: React.MutableRefObject<BottomSheetModalMethods | null> =
+    React.useRef<BottomSheetModalMethods | null>(null);
   const router: ExpoRouter.Router = useRouter();
+  const removesPlaylistButtonDisabled: boolean = !list.totalPlaylist;
 
   return (
-    <Drawer modalRef={props.modalRef} snapPoints={["27%"]}>
-      <View style={styles.wrapper}>
-        <PlaylistHeaderOptionsItem
-          onPress={() => console.log("test")}
-          icon={AlbumSVG}
-          title="Add Playlist"
-        />
-        <PlaylistHeaderOptionsItem
-          onPress={() => {
-            router.push("/favorite");
-            props.modalRef.current?.close();
-          }}
-          icon={HeartSVG}
-          title="Favorites Music"
-        />
-        <PlaylistHeaderOptionsItem
-          onPress={() => console.log("test")}
-          icon={TrashSVG}
-          title="Removes Playlist"
-          textStyle={{
-            color: destructiveColor,
-          }}
-        />
-      </View>
-    </Drawer>
+    <>
+      <Drawer modalRef={props.modalRef} snapPoints={["27%"]}>
+        <View style={styles.wrapper}>
+          <PlaylistHeaderOptionsItem
+            onPress={() => console.log("test")}
+            icon={AlbumSVG}
+            title="Add Playlist"
+          />
+          <PlaylistHeaderOptionsItem
+            onPress={() => {
+              router.push("/favorite");
+              props.modalRef.current?.close();
+            }}
+            icon={HeartSVG}
+            title="Favorites Music"
+          />
+          <PlaylistHeaderOptionsItem
+            style={{
+              opacity: removesPlaylistButtonDisabled ? 0.55 : 1,
+            }}
+            disabled={removesPlaylistButtonDisabled}
+            onPress={() => removesPlaylistDrawerRef.current?.present()}
+            icon={TrashSVG}
+            title="Removes Playlist"
+            textStyle={{
+              color: destructiveColor,
+            }}
+          />
+        </View>
+      </Drawer>
+      <RemovesAllPlaylist modalRef={removesPlaylistDrawerRef} />
+    </>
   );
 }
 
@@ -62,9 +79,10 @@ export function PlaylistHeaderOptionsItem(
 ): React.JSX.Element {
   return (
     <TouchableHighlight
+      {...props}
       onPress={props.onPress}
       underlayColor={underlayColor}
-      style={styles.listWrapper}
+      style={[styles.listWrapper, props.style]}
     >
       <>
         <props.icon width={22} height={22} />
