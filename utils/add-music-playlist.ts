441d8df3ba23990940860c45ccd6759958c15fef
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-05-29 09:05:50 -0400

    Added add music playlist utils

---
+++ utils/add-music-playlist.ts
@@ -0,0 +1,24 @@
+import { Playlist, PlaylistScheme } from "@/interfaces/playlist";
+import PlaylistRepository from "@/repository/playlist.repository";
+import * as MediaLibrary from "expo-media-library";
+
+export default function addMusicPlaylist(
+  playlist: Playlist,
+  music: MediaLibrary.Asset
+): void {
+  const latestPlaylistState: PlaylistScheme = JSON.parse(
+    PlaylistRepository.getPlaylist()
+  );
+
+  const playlistIndex: number = latestPlaylistState.playlist
+    .map((state) => state.id)
+    .indexOf(playlist.id);
+
+  latestPlaylistState.playlist.splice(playlistIndex, 1, {
+    ...playlist,
+    songs: [...playlist.songs, music],
+    totalSongs: playlist.totalSongs + 1,
+  });
+
+  PlaylistRepository.setPlaylist(latestPlaylistState);
+}
