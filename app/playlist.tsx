4c21df9914cac2f874ebe2f6514d520044d6e3a6
ItsNvaa <kadeknovacahataputra@gmail.com>
2024-05-28 21:28:05 -0400

    Added application playlist screen

---
+++ app/playlist.tsx
@@ -0,0 +1,11 @@
+import Text from "@/components/atomics/text";
+import React from "react";
+import { View } from "react-native";
+
+export default function Playlist(): React.JSX.Element {
+  return (
+    <View>
+      <Text>Playlist</Text>
+    </View>
+  );
+}
