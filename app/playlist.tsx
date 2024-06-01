34e11a5be9c6fa482183f4c729f6eaed8e3c9b2f
Novanaa <kadeknovacahayaputra@gmail.com>
2024-05-31 23:54:51 -0400

    Fixed aren't refreshed after remove a music from playlist

--- app/playlist.tsx
+++ app/playlist.tsx
@@ -1,6 +1,8 @@
 import PlaylistMusic from "@/components/atomics/playlist-music";
 import { EmptyPlaylistMusic } from "@/components/molecules/not-found";
-import { Playlist as IPlaylist } from "@/interfaces/playlist";
+import { Playlist as IPlaylist, PlaylistScheme } from "@/interfaces/playlist";
+import { usePlaylistStore } from "@/stores/playlist";
+import getPlaylistIndex from "@/utils/get-playlist-index";
 import { useLocalSearchParams } from "expo-router";
 import React from "react";
 import { FlatList, StyleSheet, View } from "react-native";
@@ -10,10 +12,22 @@ interface PlaylistSearchParams {
 }
 
 export default function Playlist(): React.JSX.Element {
+  const list: PlaylistScheme = usePlaylistStore((state) =>
+    JSON.parse(state.playlist)
+  );
   const params: PlaylistSearchParams =
     // @ts-expect-error interface conflict
     useLocalSearchParams() as PlaylistSearchParams;
-  const playlist: IPlaylist = JSON.parse(params.item);
+  const playlistItem: IPlaylist = JSON.parse(params.item);
+  const playlistIndex: number = React.useMemo(
+    () =>
+      getPlaylistIndex({
+        list,
+        playlistId: playlistItem.id,
+      }),
+    [list.playlist]
+  );
+  const playlist: IPlaylist = list.playlist[playlistIndex];
 
   if (!playlist.totalSongs) return <EmptyPlaylistMusic playlist={playlist} />;
 
