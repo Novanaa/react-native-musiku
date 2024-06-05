3a98228fd2157ef0110291ebb13418e6e615f146
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-05 06:02:31 -0400

    Realocated home cards data resources

--- components/molecules/home-cards.tsx
+++ components/molecules/home-cards.tsx
@@ -2,9 +2,36 @@ import React from "react";
 import { FlatList, Linking } from "react-native";
 import Card from "../atomics/card";
 import { rowsGap } from "@/constants/styles";
-import { homeCardsData } from "@/resources/cards";
+import SparklesSVG from "@/assets/icons/sparkles.svg";
+import CommandLineSVG from "@/assets/icons/command-line.svg";
+import HeartSVG from "@/assets/icons/heart.svg";
 
 export default function HomeCards(): React.JSX.Element {
+  const homeCardsData = React.useMemo(
+    () => [
+      {
+        title: "Rate our apps!",
+        link: "",
+        description:
+          "Hey, could you give me some stars? i will appreciate that!",
+        icon: SparklesSVG,
+      },
+      {
+        title: "Report some bugs",
+        link: "",
+        description: "Have an issues while using this application?",
+        icon: CommandLineSVG,
+      },
+      {
+        title: "Give a feedback!",
+        link: "",
+        description: "Contribute to musiku app development!",
+        icon: HeartSVG,
+      },
+    ],
+    []
+  );
+
   return (
     <FlatList
       data={homeCardsData}
