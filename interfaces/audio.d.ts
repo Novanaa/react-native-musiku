4967a8d015b7afc74e4b2324bbeb79a90510376b
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-05 23:58:57 -0400

    Removed sound object interface

--- interfaces/audio.d.ts
+++ interfaces/audio.d.ts
@@ -1,9 +1,4 @@
-import { Audio, AVPlaybackStatusSuccess } from "expo-av";
 import * as MediaLibrary from "expo-media-library";
-
-export interface SoundObject extends Audio.SoundObject {
-  status: AVPlaybackStatusSuccess;
-}
 
 export interface CurrentMusicPlayed {
   music: MediaLibrary.Asset;
