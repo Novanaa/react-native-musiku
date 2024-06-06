c893e16aeb5dfdb324ef08864ba6a3a4113e1d17
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-06 01:01:59 -0400

    Added android behavior to track player update options

--- utils/setup-track-player.ts
+++ utils/setup-track-player.ts
@@ -1,4 +1,8 @@
-import TrackPlayer, { Capability, RepeatMode } from "react-native-track-player";
+import TrackPlayer, {
+  AppKilledPlaybackBehavior,
+  Capability,
+  RepeatMode,
+} from "react-native-track-player";
 
 export default async function setupTrackPlayer() {
   await TrackPlayer.setupPlayer();
@@ -6,6 +10,10 @@ export default async function setupTrackPlayer() {
     capabilities: [Capability.Play, Capability.Pause, Capability.Stop],
     // Capabilities that will show up when the notification is in the compact form on Android
     compactCapabilities: [Capability.Play, Capability.Pause],
+    android: {
+      appKilledPlaybackBehavior:
+        AppKilledPlaybackBehavior.StopPlaybackAndRemoveNotification,
+    },
   });
   await TrackPlayer.setRepeatMode(RepeatMode.Track);
 }
